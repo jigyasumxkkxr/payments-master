1b1354a542e55df6b245c4c834121966230c5030
Jigyasu <133757043+jigyasumxkkxr@users.noreply.github.com>
2024-07-27 12:00:45 +0530

    review page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import { Signin } from "./pages/signin"
 import { Dashboard } from "./pages/dashboard"
 import { Send } from "./pages/send"
 import { Landing } from "./pages/landing"
+import { Review } from "./pages/review"
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
             <Route path="/signup" element={<Signup />} />
             <Route path="/signin" element={<Signin />} />
             <Route path="/" element={<Landing />} />
+            <Route path="/review" element={<Review />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/send" element={<Send />} />
           </Routes>
